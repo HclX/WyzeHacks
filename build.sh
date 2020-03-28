8ecb5986a88d7d0c5d36c04ef37a575c2afc3ae7
Xuan <xuan@live.com>
2020-03-28 21:31:30 +0000

    Updating the release script.

--- build.sh
+++ build.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 . wyze_hack/hack_ver.inc
 
+RELEASE=`echo "$WYZEHACK_VER"|tr '.' '_'`
+mkdir -p ./release/$RELEASE
+
 mkdir -p .tmp/Upgrade
 echo FWGRADEUP=app > .tmp/Upgrade/PARA
-
 sed "s/__WYZEHACK_VER__/$WYZEHACK_VER/g" wyze_hack.sh > .tmp/Upgrade/wyze_hack.sh
-tar -cvz -O wyze_hack >>.tmp/Upgrade/wyze_hack.sh
-
+tar --sort=name --owner=root:0 --group=root:0 --mtime='1970-01-01' -cz -O wyze_hack >>.tmp/Upgrade/wyze_hack.sh
 cp upgraderun.sh .tmp/Upgrade/
+tar --sort=name --owner=root:0 --group=root:0 --mtime='1970-01-01' -cf ./release/$RELEASE/FIRMWARE_660R.bin -C ./.tmp Upgrade
+rm -rf .tmp
 
-tar -cvf ./release/FIRMWARE_660R.bin -C ./.tmp Upgrade
-
+cp release/config.inc ./release/$RELEASE/
 MD5=`md5sum ./release/FIRMWARE_660R.bin | grep -oE "^[0-9a-f]*"`
 
-cat > ./release/version.ini << EOL
+cat > ./release/$RELEASE/version.ini << EOL
 [SD]
 version=9.9.9.9
 type=1
 md5=$MD5
 EOL
 
-rm -rf .tmp
+zip -j ./release/wyze_hacks_$RELEASE.zip \
+    ./release/$RELEASE/FIRMWARE_660R.bin \
+    ./release/$RELEASE/version.ini \
+    ./config.inc.TEMPLATE \
+    ./install.sh \
